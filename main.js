14cdc6dfdda194e745b941b7cdf669a8b838a8e2
spec <77427029+specowos@users.noreply.github.com>
2023-01-07 04:08:20 -0600

    make it less loud -ches

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var song = document.getElementById("song"); 
 var video = document.getElementById("video"); 
 
+song.volume = 0.25; 
+
 function start() {
     document.getElementById("start").style.display = "none";
     document.getElementById("content").style.display = "block";
