e10c6faab15387076c39e57da7ea8f2b476c5221
scotdotwtf <77427029+scotdotwtf@users.noreply.github.com>
2024-09-06 18:43:57 -0500

    I think i fixed video lol

--- main.js
+++ main.js
@@ -7,4 +7,5 @@ function start() {
     document.getElementById("start").style.display = "none";
     document.getElementById("content").style.display = "block";
     song.play(); 
+    video.play(); 
 }
